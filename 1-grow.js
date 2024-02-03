022d54f36cd6342890b7e59e53cc38fa084f87e2
RayhanAsauqi <reyhanalsauqi12@gmail.com>
2024-02-03 18:36:57 +0700

    day 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,7 @@
+// Challenge coding 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
 // function grow(x){
 //     return x.reduce((acc, curr) => acc * curr, 1 )
 // }
@@ -18,5 +22,5 @@
 // console.log(grow([4, 1, 1, 1, 4])) // 16
 
 const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
- console.log(grow([1, 2, 3, 4])) // 6
- console.log(grow([4, 1, 1, 1, 4])) // 16
+console.log(grow([1, 2, 3, 4])); // 6
+console.log(grow([4, 1, 1, 1, 4])); // 16
