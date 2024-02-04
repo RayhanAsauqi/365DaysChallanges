7d70e8092f60af5a5db2cabefc8becd01b0abf26
RayhanAsauqi <reyhanalsauqi12@gmail.com>
2024-02-04 17:08:51 +0700

    day 2/366

---
+++ 2-fundamentalReturn.js
@@ -0,0 +1,32 @@
+// Challenge coding 2024
+// 2/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function mod(a, b) {
+    return a % b;
+}
+
+function exponent(a, b) {
+    return Math.pow(a, b);
+}
+
+function subt(a, b) {
+    return a - b;
+}
+
+// Contoh penggunaan fungsi-fungsi tersebut:
+
