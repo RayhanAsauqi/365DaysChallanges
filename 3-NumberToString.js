b603e90dc43199dcf574c4022db1bb39557a9cd8
RayhanAsauqi <reyhanalsauqi12@gmail.com>
2024-02-05 18:42:28 +0700

    day 3/366

---
+++ 3-NumberToString.js
@@ -0,0 +1,13 @@
+// Challenge coding 2024
+// 3/366
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// function numberToString(num) {
+//   let text = num.toString();
+//   return text;
+// }
+
+// console.log(numberToString(12));
+
+const numberToString = (num) => String(num);
+console.log(numberToString(4));
