a6410531ba641b52a7e190137d928f023bc89112
RayhanAsauqi <reyhanalsauqi12@gmail.com>
2024-02-09 20:47:14 +0700

    day 5/366

---
+++ 5-fixTheMeerkat.js
@@ -0,0 +1,9 @@
+// Challenge coding 2024
+// 5/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"]));
