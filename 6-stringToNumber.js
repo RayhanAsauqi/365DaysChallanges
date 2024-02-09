f48fa3c75a76dd4d6a0f303282e2accc27daba9c
RayhanAsauqi <reyhanalsauqi12@gmail.com>
2024-02-09 20:48:02 +0700

    day 6/366

---
+++ 6-stringToNumber.js
@@ -0,0 +1,8 @@
+// Challenge coding 2024
+// 6/366
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+const stringToNumber = function (str) {
+  return parseInt(str);
+};
+
+console.log(stringToNumber("12"));
